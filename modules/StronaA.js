04100f773fd760ec0ae6cdd53ca4b585dc13b2d1
monika@grzegorska.pro <patryk.borowa@live.com>
2018-07-19 15:22:01 +0200

    new live

--- modules/StronaA.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default React.createClass({
-    render() {
-        return <div {...this.props} className="stronaa">Strona A</div>
-  }
-})
--- modules/StronaC.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default React.createClass({
-    render() {
-        return <div {...this.props} className="stronac">Strona C</div>
-  }
-})
