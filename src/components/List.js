ca6cfcab278e86bab8f45cdb439825b14898f918
monika@grzegorska.pro <patryk.borowa@live.com>
2018-07-25 14:58:12 +0200

    List working

--- src/components/List.js
+++ src/components/List.js
@@ -1,24 +1,18 @@
 ﻿import React from "react";
 import styles from "./css/List.css";
 
-class List extends React.Component {
-    render() {
-        let restaurants = [
-            { photo: <a href="https://cdn.pizzaportal.pl/20000239/logo/e04718c2fef2c41c72b6e4b431d16e26/logo.png"/> , name: "Bistro Pado", type: "Pizza", minprice: "19 zł", delivery: "5 zł", distance: "2.5 km" },
-        ];
 
-        return (
-            <div className="list">
-            <ul>
-                {restaurants.map(restaurant => (
-                    <li>
-                        {restaurant.photo} Nazwa {restaurant.name} typ {resturant.type} Cena minialna  {restaurant.minprice} Dostawa  {restaurant.delivery} Dystans {restaurant.distance} 
-          </li>
-                ))}
-            </ul>
-                </div>
-        );
-    }
-}
-
+function List() {
+    let restaurants = [
+     { photo: <img  src="https://cdn.pizzaportal.pl/20000239/logo/e04718c2fef2c41c72b6e4b431d16e26/logo.png"/> , name: "Bistro Pado", type: "Pizza", minprice: "19 zł", delivery: "5 zł", distance: "2.5 km" },
+    ];
+    const listItems = restaurants.map((restaurant) =>
+        <li>{restaurant.photo} Nazwa {restaurant.name} typ {restaurant.type} Cena minialna  {restaurant.minprice} Dostawa  {restaurant.delivery} Dystans {restaurant.distance}</li>
+    );
+    return (
+        <div className = "list">
+            <ul>{listItems}</ul>
+            </div>
+    );
+};
 export default List;
